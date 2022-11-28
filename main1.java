463be9d7539690fd08436493b354ffd76f971d66
Prema-Rahuraman <88416214+Prema-Rahuraman@users.noreply.github.com>
2022-11-28 14:23:23 +0530

    Hello World Application program updated

--- main1.java
+++ main1.java
@@ -1,5 +1,6 @@
 class main1 {
     public static void main(String[] args) {
+        System.out.println("----------------Welcome to cloud model lab----------------"); 
         System.out.println("Hello World Web Application"); 
     }
 }
